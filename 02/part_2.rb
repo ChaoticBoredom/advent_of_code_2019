d656a301128328d1247654a3a371b16c930a663f
Susan Wright <wright1@ualberta.ca>
2019-12-04 19:06:19 -0700

    Solved part_2

---
+++ 02/part_2.rb
@@ -0,0 +1,19 @@
+input = File.open("input.txt") { |data| data.readline.strip.split(",").map(&:to_i) }
+
+input[1] = 20
+input[2] = 3
+
+input.each_slice(4) do |part|
+  puts part.join(",")
+  break if part[0] == 99
+
+  new_val = if part[0] == 1
+              input[part[1]] + input[part[2]]
+            elsif part[0] == 2
+              input[part[1]] * input[part[2]]
+            end
+  input[part[3]] = new_val
+end
+
+puts input[0]
+puts input[0] == 19_690_720
