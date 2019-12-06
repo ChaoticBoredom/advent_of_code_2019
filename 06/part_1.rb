ab00f6bacdb4ac3b8c036b1730e195a93e8143ba
Susan Wright <wright1@ualberta.ca>
2019-12-05 23:38:21 -0700

    Add dijkstra components

--- 06/part_1.rb
+++ 06/part_1.rb
@@ -1,18 +1,22 @@
 require "pry"
 require "rgl/adjacency"
+require "rgl/dijkstra"
 require "rgl/traversal"
 require "rgl/graph_visitor"
+require "rgl/dijkstra_visitor"
 
 orbits = File.readlines("input.txt").map { |l| l.strip.split(")") }
 
 graph = RGL::AdjacencyGraph.new
 
+weights = {}
 suns = []
 orbiters = []
 orbits.each do |s, p|
   suns << s
   orbiters << p
   graph.add_edge s, p
+  weights[[s, p]] = 1
 end
 
 root = suns.uniq - orbiters.uniq
@@ -21,4 +25,10 @@ visitor = RGL::DFSVisitor.new(graph)
 visitor.attach_distance_map
 graph.depth_first_visit(root.first, visitor) { |_| }
 
-puts graph.vertices.sum { |v| visitor.distance_to_root(v) }
+sum = graph.vertices.sum { |v| visitor.distance_to_root(v) }
+puts sum
+
+min_visitor = RGL::DijkstraVisitor.new(graph)
+min_visitor.attach_distance_map
+graph.dijkstra_shortest_path(weights, "YOU", "SAN", min_visitor) { |_| }
+puts min_visitor.distance_map["SAN"] - 2 # Orbital hops is not the same as min distance
