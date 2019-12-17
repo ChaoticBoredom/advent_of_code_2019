3a283ba541d51814aeb4e107d9b2aef96c7a1269
Susan Wright <wright1@ualberta.ca>
2019-12-16 23:42:11 -0700

    Solved part 2, partially manually

--- 17/part_1.rb
+++ 17/part_1.rb
@@ -1,19 +1,28 @@
 require "pry"
 require_relative "../intcode"
 
+def translate_map(map)
+  map.
+    join.
+    gsub(/35/, "#").
+    gsub(/46/, ".").
+    gsub(/10/, "\n").
+    gsub(/60/, "<").
+    gsub(/62/, ">").
+    gsub(/94/, "^").
+    chomp
+end
+
 initial_input = File.open("input.txt") { |i| i.readline.strip.split(",").map(&:to_i) }
 
 computer = IntCode.new(initial_input.dup)
 
 output = []
-until computer.finished
-  output << computer.compute(stop_on_output: true).output
-end
+output << computer.compute(stop_on_output: true).output until computer.finished
 
-out_map = output.join.gsub(/35/, "#").gsub(/46/, ".").gsub(/10/, "\n").gsub(/60/, "<").gsub(/62/, ">").gsub(/94/, "^").chomp
+out_map = translate_map(output)
 
 out_array = out_map.split("\n")
-puts out_map
 
 sum = 0
 out_array.each.with_index do |row, i|
@@ -29,5 +38,26 @@ out_array.each.with_index do |row, i|
   end
 end
 
+puts out_map
+
 puts "PART 1"
 puts sum
+
+initial_input[0] = 2
+computer = IntCode.new(initial_input)
+
+path_a = "L,12,R,4,R,4,L,6\n".bytes
+path_b = "L,10,L,6,R,4\n".bytes
+path_c = "L,12,R,4,R,4,R,12\n".bytes
+main_routine = "A,C,A,B,A,C,B,C,B,A\n".bytes
+visual_feed = "n\n".bytes
+
+[main_routine, path_a, path_b, path_c, visual_feed].each do |input|
+  input.each do |v|
+    computer.add_input(v)
+  end
+end
+computer.compute
+
+puts "PART 2"
+puts computer.output
